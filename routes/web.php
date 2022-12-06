c7c7894a67257f4e8905458df4ead94a40c0199f
Sanyuaung <sanyuaung.ygn.mm.com>
2022-12-06 11:30:57 +0630

    route not include API

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
-
+  
 use Illuminate\Support\Facades\Route;
-
+  
+use App\Http\Controllers\ProductController;
+  
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+    
+Route::resource('products', ProductController::class);
